0ffb63d715607d487a50c06f26e21465ae6ddfd2
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-08-25 12:28:52 -0500

    Argegando expresiones reuglares

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
 
 import news_page_objects as news
 from common import config
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https://.+/.+$') #https://example.com/hello
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
@@ -14,7 +16,19 @@ def _news_scraper(news_site_uid):
     homepage = news.HomePage(news_site_uid, host)
 
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except:
+        pass
+
+def _build_link(host, link):
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
